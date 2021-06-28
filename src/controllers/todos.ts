2822bbbdc6ec0401be5a9027fb15cf77ba6b21d2
ganciaux <ghislain.anciaux@live.fr>
2021-06-28 07:44:45 +0200

    Create model, routes and controller

---
+++ src/controllers/todos.ts
@@ -0,0 +1,12 @@
+//const express = require('express');
+//import { Request, Response, NextFunction} from 'express'
+import { RequestHandler } from 'express'
+import { Todo } from '../models/todo'
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+    const text = (req.body as { text: string }).text;
+    const newTodo = new Todo(Math.random().toString(), text);
+    TODOS.push(newTodo);
+    res.status(201).json({ message: 'created new todo', createTodo: newTodo });
+};
